188ef68a44701c23f1b5107927def702575c2702
Alexis Chaves Pereira <alexis.moventes@gmail.com>
2019-11-19 10:48:34 +0100

    init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,49 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  bruitConfig: {
+    apiKey: "44a63ba8-d93b-484f-b22f-15bcf45851c8",
+    labels: {
+      title: "Send us a feedback",
+      introduction: "Send us a feedback with bruit.io..."
+    },
+    form: [
+      {
+        label: "Question ?",
+        type: "text",
+        id: "title"
+      },
+      {
+        label: "Rate",
+        type: "rating",
+        required: true,
+        max: 5
+      },
+      {
+        label: "Your email",
+        type: "email",
+        required: true
+      },
+      {
+        label: "testtest",
+        type: "select",
+        required: true,
+        options: ['a', 'b', 'c']
+      },
+      {
+        label: "Lorem",
+        type: "textarea",
+        required: true
+      },
+      {
+        id: "agreement",
+        type: "checkbox",
+        label: "I agree to send technical information in addition to my answers",
+        value: null
+      }
+    ]
+  }
 };
 
 /*
